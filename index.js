2a98491dd24e4221eab3269de5dbb7f4eee66516
jimmiezhang <wengeezhang@gmail.com>
2018-12-22 15:20:01 +0800

    返回内容修改

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 80268;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('1111');
 });
 
 server.listen(port, hostname, () => {
